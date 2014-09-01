e2a69511022a57f0ae881d87bea771a0cba7f32a
jcorner <tcanto88@yahoo.com.br>
2014-09-01 14:36:14 -0300

    Ajuste dos nomes para melhor organização (correção dos nomes)

---
+++ JogoRpg/src/interface/Janela.java
@@ -0,0 +1,42 @@
+package interfce;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import panels.PaneInicio;
+
+public class Janela extends JFrame{
+	
+	public Janela(){
+	
+	MenuJan menuBar = new MenuJan();	
+	
+		
+	setLayout(null);	
+	setTitle("Tela Inicial");
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setSize(800, 600);
+	setLocationRelativeTo(null);
+	setResizable(false);
+	setVisible(true);
+	
+	//getContentPane().add(paneLogin, BorderLayout.CENTER);
+	setJMenuBar(menuBar);
+	/*static PaneInicio teste = new PaneInicio();
+	
+	JFrame Inicio = new JFrame("Tela Inicial");
+	Inicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
+	Inicio.getContentPane().add(teste,BorderLayout.CENTER);
+	Inicio.pack();
+	Inicio.setVisible(true);*/
+	
+	
+	}
+
+	
+}
---
+++ JogoRpg/src/interface/Start.java
@@ -0,0 +1,15 @@
+package interfce;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TelaInicial();
+		
+		
+	}
+
+}
