fe4b5ba30b04475d6c80187637e5106ac9113dfe
jcorner <tcanto88@yahoo.com.br>
2014-09-03 15:28:56 -0300

    Adicionada classe principal para rodar testes Game

---
+++ JogoRpg/src/interfce/Game.java
@@ -0,0 +1,15 @@
+package interfce;
+
+import utils.Atributo.Tipo;
+import utils.Personagem;
+import utils.Atributo;
+
+public class Game {
+	private static Personagem p;
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
